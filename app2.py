c3c850798c8b63bfacfc850ca4b435f3034dadd3
Noureen AC <nourinnn1823@gmail.com>
2025-05-11 21:43:20 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -78,7 +78,7 @@ if st.button("🚀 Submit"):
         st.subheader("📸 Uploaded Image")
         st.image(image, use_container_width =True)
 
-        col1, col2 = st.columns(2)
+        col1 = st.columns(1)
 
         with col1:
             st.markdown("### 🤖 Response from `meta-llama/llama-4-scout-17b-16e-instruct`")
